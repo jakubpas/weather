6457acb6cb24b88f4dda0060b93efaa02c3a82c1
Jakub Pas <jakub.x.pas@gsk.com>
2020-03-10 16:41:06 +0100

    disable python

--- src/test/java/net/jakubpas/weather/WeatherApplicationTests.java
+++ src/test/java/net/jakubpas/weather/WeatherApplicationTests.java
@@ -1,5 +1,6 @@
 package net.jakubpas.weather;
 
+import net.jakubpas.weather.controller.GreetingController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Objects;
 
@@ -22,5 +24,10 @@ class WeatherApplicationTests {
 				"http://api.openweathermap.org/data/2.5/weather?id=7531836&appid=77cd0ebb0fe3ef249daca875ba1e65f0&units=metric"
 				, HttpMethod.GET, entity, Weather.class);
 		return Objects.requireNonNull(aResponse.getBody()).getMain().getTemperature();
+	}
+
+	@Test
+	void parseTest() {
+		assertEquals("19.93", GreetingController.parseDevice("src/main/resources/w1_slave"));
 	}
 }
