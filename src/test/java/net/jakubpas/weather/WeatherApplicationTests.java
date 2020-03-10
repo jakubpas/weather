375427d6feac710ff9481ec5f605bdfa7069ae89
Jakub Pas <jakubpas@gmail.com>
2020-03-10 23:42:33 +0100

    fix tests

--- src/test/java/net/jakubpas/weather/WeatherApplicationTests.java
+++ src/test/java/net/jakubpas/weather/WeatherApplicationTests.java
@@ -16,17 +16,6 @@ import java.util.Objects;
 class WeatherApplicationTests {
 
 	@Test
-	String contextLoads() {
-		HttpHeaders httpHeaders = new HttpHeaders();
-		RestTemplate restTemplate = new RestTemplate();
-		HttpEntity<Weather> entity = new HttpEntity<>(httpHeaders);
-		ResponseEntity<Weather> aResponse = restTemplate.exchange(
-				"http://api.openweathermap.org/data/2.5/weather?id=7531836&appid=77cd0ebb0fe3ef249daca875ba1e65f0&units=metric"
-				, HttpMethod.GET, entity, Weather.class);
-		return Objects.requireNonNull(aResponse.getBody()).getMain().getTemperature();
-	}
-
-	@Test
 	void parseTest() {
 		assertEquals("19.93", GreetingController.parseDevice("src/main/resources/w1_slave"));
 	}
